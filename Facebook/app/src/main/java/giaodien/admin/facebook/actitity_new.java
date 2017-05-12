e36ce2dbfe04cad75d43cc7d82798f31a3ba5845
Gooner11 <ngoctrung150695@gmail.com>
2017-05-12 20:22:23 +0700

    update_intent

---
+++ Facebook/app/src/main/java/giaodien/admin/facebook/actitity_new.java
@@ -0,0 +1,19 @@
+package giaodien.admin.facebook;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class actitity_new extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actitity_new);
+
+    }
+
+}
